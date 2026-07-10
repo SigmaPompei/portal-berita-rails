e04c78d047c5c9d170bf7f4895f2b50fdd02cb36
SigmaPompei <rickyfernandojulian@gmail.com>
2026-07-10 13:45:50 +0700

    Fix environment.rb config

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.force_ssl = false  # Railway handle SSL sendiri
-config.assets.compile = true
-config.public_file_server.enabled = true
