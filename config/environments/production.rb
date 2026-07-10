f1367558998263c25d5714ad01e0dec92a55bf46
SigmaPompei <rickyfernandojulian@gmail.com>
2026-07-10 13:42:11 +0700

    Fix production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+config.force_ssl = false
+config.assets.compile = true
+config.public_file_server.enabled = true
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
