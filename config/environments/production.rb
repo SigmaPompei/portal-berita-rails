4f9336ac95a10f4991bafa8a3a49cbaf3f7753a6
SigmaPompei <rickyfernandojulian@gmail.com>
2026-07-10 13:54:16 +0700

    Fix environment.rb config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  Rails.application.configure do
   config.force_ssl = false
   config.assets.compile = true
   config.public_file_server.enabled = true
