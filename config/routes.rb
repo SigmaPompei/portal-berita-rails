1a8751a502e655e9e8f7f0d9ed15ae0f3f1995ae
SigmaPompei <rickyfernandojulian@gmail.com>
2026-07-26 11:38:01 +0700

    Fix routes profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  get "profiles/show"
   devise_for :users, controllers: { registrations: "registrations" }
   root 'pages#index'
+
   resources :articles do
     resources :comments, only: [:create, :destroy]
-    resources :profiles, only: [:show]
   end
+
+  resources :profiles, only: [:show]
   resources :categories, only: [:index, :show]
 
   namespace :admin do
     get '/', to: 'dashboard#index', as: :dashboard
     resources :articles, only: [:index, :destroy]
   end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
